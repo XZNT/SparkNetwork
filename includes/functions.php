e3c00331d2f90df8a8cc6e1d17a4c4d984d057b0
XZNT <70913080+XZNT@users.noreply.github.com>
2023-12-08 22:30:43 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -534,7 +534,7 @@ function _error()
                                 <li>" . "Are you sure that you have typed the correct hostname?" . "</li>
                                 <li>" . "Are you sure that the database server is running?" . "</li>
                             </ul>
-                            <p>" . "If you're unsure what these terms mean you should probably contact your host. If you still need help you can always visit the" . " <a href='http://support.zamblek.com'>" . "Sngine Support" . ".</a></p>
+                            <p>" . "If you're unsure what these terms mean you should probably contact your host. If you still need help you can always visit the" . " <a href='https://yruz.one/Zanto'>" . "YRUZ Support" . ".</a></p>
                             </div>";
         break;
 
